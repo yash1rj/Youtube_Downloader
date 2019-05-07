a3d50775a41ae2e1b8713b3983d968e8f05fd077
yash1rj <yashrj130@gmail.com>
2019-05-08 00:46:31 +0530

    Update background.js

--- src/background.js
+++ src/background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onMessage.addListener(function(request, sender, callback) {
     console.log("Received ", request, sender, callback);
 
-    chrome.downloads.download({url: request.url, filename: request.name.split(";")[0].replace("|", "")});
+    chrome.downloads.download({url: request.url, filename: request.name.split(";")[0].replace(/[^a-zA-Z0-9.]/g, "")});
 }); 
