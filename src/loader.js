924676bba82cee33f5d28c9b83a7da9ff82bcdb8
yash1rj <yashrj130@gmail.com>
2019-05-07 23:57:38 +0530

    Update loader.js

--- src/loader.js
+++ src/loader.js
@@ -1,9 +1,19 @@
 var s = document.createElement("script");
-s.src = chrome.extension.getURL("./src/background.js");
+s.src = chrome.extension.getURL("src/youtubedl.js");
 
 
 (document.head || document.documentElement).appendChild(s);
 
 window.addEventListener("message", function(e) {
-    console.log(e);
+    console.log("download: ", e);
+
+    var ext = e.data.type.split("/")[1].split(":")[0];
+    var fn = e.data.name + "." + ext;
+
+    console.log(fn, chrome);
+
+    chrome.runtime.sendMessage({name: fn, url: e.data.url}, function(res) {
+        console.log("result: ",res);
+    });
+    // chrome.downloads.download({url: e.data.url, filename: fn});
 })
