ffcf5e02d8299a3ba70e561d73b704efe4b3be49
yash1rj <yashrj130@gmail.com>
2019-05-08 00:46:34 +0530

    Update loader.js

--- src/loader.js
+++ src/loader.js
@@ -5,7 +5,7 @@ s.src = chrome.extension.getURL("src/youtubedl.js");
 (document.head || document.documentElement).appendChild(s);
 
 window.addEventListener("message", function(e) {
-    console.log("download: ", e);
+    // console.log("download: ", e);
 
     var ext = e.data.type.split("/")[1].split(":")[0];
     var fn = e.data.name + "." + ext;
@@ -15,5 +15,4 @@ window.addEventListener("message", function(e) {
     chrome.runtime.sendMessage({name: fn, url: e.data.url}, function(res) {
         console.log("result: ",res);
     });
-    // chrome.downloads.download({url: e.data.url, filename: fn});
 })
